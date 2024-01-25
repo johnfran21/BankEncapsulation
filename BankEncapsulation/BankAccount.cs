01d0ad2800e805f9c90e7e22c2599d061fca5b8d
John Franzese <johnfranzese21@gmail.com>
2024-01-25 12:57:43 -0600

    BankEncapsulation Updates

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	internal class BankAccount
+	{
+		private double _balance = 0.0;
+
+		public void Deposit(double money)
+		{
+			_balance += money;
+		}
+
+		public double GetBalance()
+		{
+            Console.WriteLine($"{_balance}");
+			return _balance;
+			
+		}
+	}
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            Console.WriteLine("Welcome New Customer");
+            Console.WriteLine("Please Enter Deposit Amount");
+
+            account.Deposit(25.5);
+            account.GetBalance();
+  
+            
         }
     }
 }
